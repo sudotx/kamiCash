c0c7e26be131d0f43a958fb7c8811bf7445f3ffd
sudotx <64108848+sudotx@users.noreply.github.com>
2024-12-25 22:04:30 +0100

    rename auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import { verifyJwt } from "../utils/jwt";
 
-export const requireAdminAuth = async (
+export const requireAuth = async (
     req: Request,
     res: Response,
     next: NextFunction
--- src/resources/User/user.routes.ts
+++ src/resources/User/user.routes.ts
@@ -2,11 +2,11 @@ import { Router } from "express";
 import validateResource from "../../middlewares/validate-resource";
 import { getUserBalance, getUserProfile, getUserTransactions } from "./controllers/user.controller";
 import { getUserBalanceSchema, getUserProfileSchema, getUserTransactionsSchema } from "./schemas/index.schema";
-import { requireAdminAuth } from "../../middlewares/auth.middleware";
+import { requireAuth } from "../../middlewares/auth.middleware";
 
 const userRouter = Router();
 
-userRouter.get("/profile", validateResource(getUserProfileSchema), requireAdminAuth, getUserProfile);
+userRouter.get("/profile", validateResource(getUserProfileSchema), requireAuth, getUserProfile);
 
 userRouter.get("/balance", validateResource(getUserBalanceSchema), getUserBalance);
 
